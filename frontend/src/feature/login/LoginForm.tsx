3c868e01d1aed66df027b67bb02e1a81937e6935
stryer2002 <albertomonsa.10@gmail.com>
2025-08-24 01:27:56 -0500

    wip:se adelanta valicación de los campos del login

--- frontend/src/feature/login/LoginForm.tsx
+++ frontend/src/feature/login/LoginForm.tsx
@@ -1,60 +1,81 @@
+import { singInSchema } from "@/schemas";
 import { useFormik } from "formik";
-import { signIn } from "next-auth/react";
-import { useEffect } from "react";
+
+interface SingInInterface {
+	email: string;
+	password: string;
+}
 
 export default function LoginForm() {
-	const onSubmit = async (e: any) => {
-		e.preventDefault();
-		const { email, password } = values;
+	const onSubmit = async (values: SingInInterface) => {
+		console.log(values);
 
-		await signIn("credentials", {
-			email,
-			password,
-			callbackUrl: "/",
-		});
+		// await signIn("credentials", {
+		// 	email,
+		// 	password,
+		// 	callbackUrl: "/",
+		// });
 	};
 
-	const { values, errors, touched, handleChange, handleBlur } = useFormik({
-		initialValues: {
-			email: "",
-			password: "",
-		},
-		onSubmit,
-	});
+	const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+		useFormik({
+			initialValues: {
+				email: "",
+				password: "",
+			},
+			validationSchema: singInSchema,
+			onSubmit,
+		});
 
 	return (
-		<form action="" onSubmit={onSubmit} className="flex flex-col gap-3">
-			<label htmlFor="" className="flex flex-col gap-1">
-				<span>Correo</span>
+		<form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+			<div className="flex flex-col gap-1">
+				<label htmlFor="email" className="font-medium text-lg">
+					Correo
+				</label>
 				<input
 					id="email"
 					name="email"
 					type="text"
+					value={values.email}
 					onChange={handleChange}
 					onBlur={handleBlur}
 					placeholder="you@example.com"
-					className="px-3 py-2 placeholder:text-gray-500 outline-none ring ring-bismark-400 focus:ring-2 rounded-md"
+					className="px-3 py-2 placeholder:text-gray-500 outline-none ring ring-edtech-blue-500 focus:ring-2 rounded-md"
 				/>
-			</label>
+				{errors.email && touched.email && (
+					<p className="text-base text-edtech-error-500">
+						{errors.email}
+					</p>
+				)}
+			</div>
 
-			<label htmlFor="" className="flex flex-col gap-1">
-				<span>Contraseña</span>
+			<div className="flex flex-col gap-1">
+				<label htmlFor="password" className="font-medium text-lg">
+					Contraseña
+				</label>
 				<input
 					id="password"
 					name="password"
 					type="text"
+					value={values.password}
 					onChange={handleChange}
 					onBlur={handleBlur}
 					placeholder="Ingresa 6 caracteres o m&aacute;s"
-					className="px-3 py-2 placeholder:text-gray-500 outline-none ring ring-bismark-400 focus:ring-2 rounded-md"
+					className="px-3 py-2 placeholder:text-gray-500 outline-none ring ring-edtech-blue-500 focus:ring-2 rounded-md"
 				/>
-			</label>
+				{errors.password && touched.password && (
+					<p className="text-sm text-edtech-error-500">
+						{errors.password}
+					</p>
+				)}
+			</div>
 
 			<button
 				name="submit"
 				type="submit"
-				disabled={!values.email || !values.password}
-				className="bg-bismark-200 hover:bg-bismark-300 active:bg-bismark-400 disabled:bg-bismark-100 rounded-md py-2 text-bismark-950 disabled:text-bismark-400">
+				disabled={false}
+				className="bg-edtech-blue-800 hover:bg-edtech-blue-700 disabled:bg-edtech-blue-300 rounded-md py-2 text-bismark-950 disabled:text-bismark-400 text-white font-medium disabled:cursor-not-allowed">
 				Ingresar
 			</button>
 		</form>
---
+++ frontend/src/schemas/login.schema.ts
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+export const singInSchema = yup.object({
+	email: yup
+		.string()
+		.required("El correo es requerido")
+		.email("El correo electrónico no es válido")
+		.matches(
+			/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+			"El correo electrónico no es válido"
+		)
+		.max(255, "Máximo 255 caracteres")
+		.trim(),
+
+	password: yup
+		.string()
+		.required("La contraseña es obligatoria")
+		.matches(
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-ZñÑ0-9!@#$%^&*(),.?":{}|<>]+$/,
+			"La contraseña debe contener al menos una letra minúscula, una mayúscula, un número y no puede tener espacios"
+		)
+		.min(8, "La contraseña requiere minimo 8 caracteres")
+		.trim(),
+});
